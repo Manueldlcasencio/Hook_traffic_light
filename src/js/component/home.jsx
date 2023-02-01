f92b2e593056f2776772b583034fa86b72fd4cea
Manueldlcasencio <manuel.dlcasencio@gmail.com>
2023-02-01 19:26:18 +0000

    Solución: Ternarios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,23 +2,15 @@ import React, { useState } from 'react';
 
 const Home = () => {
 	let [color, setColor] = useState('')
-	// Div al inicio o no seleccionado
-	let colY = <div className='light yellow' onClick={() => setColor('yellow')}></div>
-	let colR = <div className='light red' onClick={() => setColor('red')}></div>
-	let colG = <div className='light green' onClick={() => setColor('green')}></div>
-	// Cambio de div al seleccionar
-	if (color=="yellow") {colY = <div className='light yellow glow-yellow' onClick={() => setColor('yellow')}></div>}
-	else if (color=="red") {colR = <div className='light red glow-red' onClick={() => setColor('red')}></div>}
-	else if (color=="green") {colG = <div className='light green glow-green' onClick={() => setColor('green')}></div>}
-	// Componente exportado
 	return (
 		<div className="contenedor">
 			<div className='traffic-light'>
-				{colR}
-				{colY}
-				{colG}
+				<div className={`light red ${color == 'red' ? 'glow-red' : ''}`}   onClick={() => setColor('red')}></div>
+				<div className={`light yellow ${color == 'yellow' ? 'glow-yellow' : ''}`} onClick={() => setColor('yellow')}></div>
+				<div className={`light green ${color == 'green' ? 'glow-green' : ''}`} onClick={() => setColor('green')}></div>
 			</div>
 		</div>
 	);
 };
 export default Home;
+
